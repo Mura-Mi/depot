52fc6447010f0f0ab37cb3fe416d32a35e026332
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2013-07-08 23:01:03 +0900

    initialized rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = '1c2d3bbaf5ef9d027f622b102571bd160525edcd1096165ae00cf51b4acf082a662ded2325b227a7be9ee0ce8854b4e77427f31f1c9e7fbae5021bdbadceed54'
