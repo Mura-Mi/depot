f715a6394b171cfd90ebfd2883b24a8c3f0752e4
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2013-07-09 22:17:36 +0900

    modified product form views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+Product.delete_all
+
+Product.create([
+{
+  title: 'iPad 4',
+  description:
+    %{iPad is the whole new tablet device.},
+  image_url: 'ipad4_2.jpg',
+  price: 399.99
+},
+{
+  title: 'iPhone5',
+  description:
+    %{iPhone5 is has 4 inch screen. <br>It's whole new product.},
+  image_url: 'iphone5.jpg',
+  price: 299.99
+}
+])
+
