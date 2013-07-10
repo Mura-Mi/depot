04c9bf41b0bcc5cd0776cd963cb1f3d51f93e760
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2013-07-10 15:22:09 +0900

    add Cart entity and line_item entity. Also added the relation information

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,11 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  # Validation methods
   validates :title, :description, :image_url, presence: true;
   validates :title, uniqueness: true;
   validates :price, numericality: {greater_than_or_equal_to: 0.01};
@@ -8,5 +14,18 @@ class Product < ActiveRecord::Base
       with: %r{\.(gif|jpg|png)$}i,
       message: 'must be either gif, jpeg or png image file.'
     }
-  validates :title, length: {minimum: 10} # too_short: 'No! It is TOO SHORT!!!!'
+  validates :title, length: {minimum: 10} 
+
+
+  private
+  def ensure_not_referenced_by_any_line_item 
+    if line_items.empty?
+      return true;
+    else
+      errors.add(:base, 'There is some line_item that reference this product.');
+      return false;
+    end
+  end
+
+
 end
