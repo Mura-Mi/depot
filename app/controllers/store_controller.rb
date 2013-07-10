c36bfbdbe5e6f161ae5f980a30a3a897d4a9f411
Takuya "Mura-Mi" Murakami <mura.mi.nantoka@gmail.com>
2013-07-11 08:32:20 +0900

    added access times counter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,16 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    @count = accessed_time;
   end
+
+  private
+  def accessed_time
+    if session[:counter].nil? then
+      return session[:counter] = 1;
+    else
+      return session[:counter] += 1;
+    end
+  end
+
 end
